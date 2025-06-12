817398a0600b0c7139e778dcca517518df004a6f
Juan Agudelo <verojuan9007@gmail.com>
2025-06-11 22:00:28 -0500

    testing and trying more scroll effects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,10 @@ import { useTransform, motion } from "framer-motion";
 import React from "react";
 
 const About = ({ scrollYProgress }) => {
-  const x = useTransform(scrollYProgress, [0.33, 0.66], [0.3, 0]);
-  
-
+  const opacity = useTransform(scrollYProgress, [0, 0.66], [0.66, 0]);
   return (
     <motion.div
-      style={{ x }}
+      style={{ opacity }}
       className=" bg-white text-2xl md:text-4xl text-black h-screen w-full flex justify-center px-6"
     >
       <div className="h-full flex flex-col md:flex-row items-center gap-8">
