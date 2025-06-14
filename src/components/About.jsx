33c3548f238131352dc59539ba6fd3070886333a
Juan Agudelo <verojuan9007@gmail.com>
2025-06-13 19:29:03 -0500

    trying scrollY and scrollYProgress framer motion on 2 componentes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,24 @@ import { useTransform, useScroll, motion } from "framer-motion";
 import React, { useRef } from "react";
 
 const About = () => {
-  
+  const about = useRef();
+  const { scrollY } = useScroll({
+    target: about,
+    offset: ["start center", "end start"],
+  });
 
-  
+  const y = useTransform(scrollY, [0, 500], [0, -500])
+
   return (
-    <motion.div
-     
-      className="bg-white text-2xl md:text-4xl text-black h-screen w-full flex justify-center px-6"
-    >
+    <motion.div 
+      ref={about}
+    className="bg-white text-2xl md:text-4xl text-black h-screen w-full flex justify-center px-6">
       <div className="h-full flex flex-col md:flex-row items-center gap-8">
-        <h1 className="font-extrabold">About Us</h1>
+        <motion.h1 
+        style={{y}}
+        className="font-extrabold">
+          About Us
+        </motion.h1>
         <p className="text-2xl md:text-3xl font-bold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero rem
           voluptates fugit tenetur reprehenderit harum, doloremque iure...
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,31 +3,31 @@ import img1 from "../img/img1.avif";
 import { useScroll, useTransform, motion } from "framer-motion";
 
 const HeroSection = () => {
-  const hero = useRef();
+  const hero = useRef(null);
   const { scrollYProgress, scrollY } = useScroll({
     target: hero,
-    offset: ["start start", "end center"],
+    offset: ["start start", "end start"],
   });
 
-  const opacity = useTransform(scrollYProgress, [0, 0.90], [1, 0]);
-  const scale = useTransform(scrollYProgress, [0, 0.9], [1, 0]);
-  const y = useTransform(scrollY, [0, 700], [0, -550]);
   
+  const opacity = useTransform(scrollYProgress, [0, 0.8], [1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 0.9], [1, 0]);
+
+ 
+  const y = useTransform(scrollY, [0, 600], [0, -550]);
 
   return (
     <motion.div
       ref={hero}
       style={{ scale, opacity, y }}
-      className="h-screen w-full flex items-center justify-center px-6 text-2xl"
+      className="h-screen w-full flex items-center justify-center px-6 text-2xl bg-amber-400"
     >
       <div className="flex flex-col md:flex-row items-center gap-8">
         <div className="text-center md:text-left max-w-xl">
-          <motion.h1 style={{scale, opacity,y }} className="font-bold mb-4">
+          <motion.h1 style={{ scale, opacity, y }} className="font-bold mb-4">
             New Release
           </motion.h1>
-          <motion.p 
-          style={{y}}
-          className="text-lg text-gray-200">
+          <motion.p style={{ y }} className="text-lg text-gray-200">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum quis
             esse quod atque mollitia minima soluta suscipit libero, architecto
             ipsum expedita deleniti ipsam? Nisi sed voluptas, inventore
@@ -35,9 +35,12 @@ const HeroSection = () => {
           </motion.p>
         </div>
 
-        <motion.img 
-        style={{y}}
-        src={img1} alt="Hero" className="w-80 h-auto rounded" />
+        <motion.img
+          style={{ y }}
+          src={img1}
+          alt="Hero"
+          className="w-80 h-auto rounded"
+        />
       </div>
     </motion.div>
   );
